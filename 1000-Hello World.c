b193b336d4aad2d6594c1e93de61dc4dd76029a9
Joydip Datta <140168640+Joydip-007@users.noreply.github.com>
2024-02-02 00:32:36 +0600

    Create 1000-Hello World.c

---
+++ 1000-Hello World.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    printf("Hello World!\n");
+ 
+    return 0;
+}
+
+
+//Don't forget to remove the space in the file name. Otherwise, the code won't run!
