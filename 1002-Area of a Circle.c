32c3649a93798eedbca9f56502efd3a493214e75
Joydip Datta <140168640+Joydip-007@users.noreply.github.com>
2024-02-02 00:57:26 +0600

    Create 1002-Area of a Circle.c

---
+++ 1002-Area of a Circle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double A, R;
+    
+    scanf("%lf", &R);
+    
+    A=3.14159*R*R;
+    
+    printf("A=%.4lf\n", A);
+ 
+    return 0;
+}
+
+
+
+
+
+//Don't forget to remove the space in the file name. Otherwise, the code won't run!
