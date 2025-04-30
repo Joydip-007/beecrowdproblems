77ea0a024b5214296620400091a42dc551d18b2b
Joydip Datta <140168640+Joydip-007@users.noreply.github.com>
2025-05-01 00:38:12 +0600

    Add files via upload

---
+++ 1009-Salary_With_Bonus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    char Name;
+    double ini_salary, sales, final_salary;
+
+    getchar();
+    scanf("%s", &Name);
+    getchar();
+    scanf("%lf", &ini_salary);
+    scanf("%lf", &sales);
+    final_salary = ini_salary + sales*.15;
+    printf("Total = R$ %.2lf\n", final_salary);
+}
