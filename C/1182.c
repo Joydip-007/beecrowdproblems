1ec9791192eca9ed2e27a0ff65f38172995a33b0
Joydip Datta <joydip.datta15@gmail.com>
2025-05-08 22:12:30 +0600

    Modified 1182.c

--- C/1182.c
+++ C/1182.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    
+    double M[12][12];
+    double sum = 0, avg=0;
+    char T;
+    int i, j, n;
+    scanf("%d", &n);
+    getchar();
+    scanf("%c", &T);
+    getchar();
+    for(i=0;i<12;i++){
+        for(j=0;j<12;j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+    for(i=0;i<12;i++){
+        sum += M[i][n];
+    }
+    if(T=='S'){
+        printf("%.1lf\n", sum);
+    }
+    else if(T=='M'){
+        avg = sum/12;
+        printf("%.1lf\n", avg);
+    }
+    return 0;
 }
